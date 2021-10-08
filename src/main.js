cf4b93d4fea0207ae242324cf72854373cbff90f
Sara Riveros <s.riveros.olmedo@gmail.com>
2021-10-08 10:38:57 -0300

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import athletes from './data/athletes/athletes.js';
+//import data from './data/athletes/athletes.json';
 // import data from './data/rickandmorty/rickandmorty.js';
+fetch('./data/athletes/athletes.json')
+   .then(resp => resp.json())
+   .then(data=> data);
+ console.log(data);
+ 
+//const athlete= data.athletes;
+//crear filtro 
+//let femaleAthletes = athlete.filter(athlete => athlete.gender=="F");
+//console.log(femaleAthletes);
+//crear una funcion que reciba a los atletas y genera la informacion para llamar a los valores contenidos
+//const div = document.getElementById("root");
+//const drawCard = (athlete)=>{
 
-console.log(example, data);
+//div.innerHTML += ` <div>
+//<ol>
+  //<li>Nombre:${athlete.name}</li>
+//</ol>
+//</div>`;
+//}
+ 
+//const femaleMedal = femaleAthletes.filter(athlete =>athlete.medal=="Gold");
+
+
+    
+fetch('./data/athletes/athletes.json')
+   .then(resp => resp.json())
+   .then(data=> console.log(data));
+
+ 
+   
+    
+  // fetch('/readme.txt')
+  // .then(response => response.text())
+  // .then(data => console.log(data));
+
+  //fetch('http://example.com/movies.json')
+  //.then(response => response.json())
+  //.then(data => console.log(data));
+
+    
